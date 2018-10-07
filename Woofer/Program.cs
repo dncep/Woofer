035a3e646f0d6a127876616112836a95ece50c4b
Energyxxer <energyxxer@gmail.com>
2018-10-06 23:16:29 -0400

    Add spikes, teleporters, a few decorations and expand pre boss

--- Woofer/Program.cs
+++ Woofer/Program.cs
@@ -10,13 +10,14 @@ namespace WooferGame
     {
         public static WooferController Controller;
 
-        public static readonly string DirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), @"Woofer");
+        public static readonly string DirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Woofer");
         public static readonly string ScenesPath = Path.Combine(DirectoryPath, "scenes");
 
         [STAThread]
         static void Main(string[] args)
         {
             GameStart.Start(Controller = new WooferController());
+
         }
     }
 }
