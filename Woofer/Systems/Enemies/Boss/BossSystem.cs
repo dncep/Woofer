636a4ccccbfbd7af9cc416e6deb443186488b5f8
Energyxxer <energyxxer@gmail.com>
2018-10-14 02:19:28 -0400

    Add basic boss AI

--- Woofer/Systems/Enemies/Boss/BossSystem.cs
+++ Woofer/Systems/Enemies/Boss/BossSystem.cs
@@ -9,28 +9,98 @@ using EntityComponentSystem.Entities;
 using EntityComponentSystem.Events;
 using EntityComponentSystem.Util;
 using WooferGame.Systems.Physics;
+using WooferGame.Systems.Player;
 
 namespace WooferGame.Systems.Enemies.Boss
 {
     [ComponentSystem("BossSystem", ProcessingCycles.Update),
-        Watching(typeof(Boss))]
+        Watching(typeof(PlayerComponent), typeof(Boss))]
     class BossSystem : ComponentSystem
     {
         public override void Update()
         {
-            foreach(Boss boss in WatchedComponents)
+            Entity player = WatchedComponents.OfType<PlayerComponent>().FirstOrDefault()?.Owner;
+            foreach(Boss boss in WatchedComponents.OfType<Boss>())
             {
                 Spatial sp = boss.Owner.GetComponent<Spatial>();
                 Physical phys = boss.Owner.GetComponent<Physical>();
+
+                Spatial playerSp = player?.GetComponent<Spatial>();
                 if (sp == null || phys == null) continue;
 
                 phys.GravityMultiplier = 1;
                 phys.Velocity = new Vector2D(0, 362) * Owner.DeltaTime;
-                boss.HoverTimer += Owner.DeltaTime;
+                /*boss.HoverTimer += Owner.DeltaTime;
 
                 Vector2D hoverVelocity = new Vector2D(0, 16 * Math.Sin(Math.PI*boss.HoverTimer/2));
 
-                phys.Velocity += hoverVelocity;
+                phys.Velocity += hoverVelocity;*/
+
+                Vector2D? flyTo = null;
+                float flySpeed = 1;
+
+                if(boss.State == Boss.Circling)
+                {
+                    if(boss.Transitioning)
+                    {
+                        if ((sp.Position - new Vector2D(0, 128)).Magnitude < 1)
+                        {
+                            boss.Transitioning = false;
+                            boss.StateData = 0;
+                        }
+                        else
+                        {
+                            flyTo = new Vector2D(0, 128);
+                            flySpeed = 2;
+                        }
+                    }
+                    if(!boss.Transitioning)
+                    {
+                        boss.StateData += Owner.DeltaTime;
+                        if(boss.StateData > 16)
+                        {
+                            boss.ChangeState(Boss.Laser);
+                        } else
+                        {
+                            phys.Velocity += new Vector2D(125.6 * Math.Cos(Math.PI * boss.StateData / 4), 0);
+                        }
+                    }
+                } else if(boss.State == Boss.Laser)
+                {
+                    if(boss.Transitioning)
+                    {
+                        if ((sp.Position - new Vector2D(0, 256)).Magnitude < 1)
+                        {
+                            boss.Transitioning = false;
+                            boss.StateData = 0;
+                        }
+                        else
+                        {
+                            flyTo = new Vector2D(0, 256);
+                            flySpeed = 2;
+                        }
+                    }
+                    if(!boss.Transitioning)
+                    {
+                        boss.StateData += Owner.DeltaTime;
+                        if (boss.StateData < 4)
+                        {
+                            if(playerSp != null)
+                            {
+                                flyTo = new Vector2D(playerSp.Position.X, sp.Position.Y);
+                            }
+                        } else
+                        {
+                            boss.ChangeState(Boss.Circling);
+                        }
+                    }
+                }
+
+                //Functions
+                if(flyTo != null)
+                {
+                    phys.Velocity += flySpeed * (flyTo.Value - sp.Position);
+                }
             }
         }
     }
--- Woofer/Systems/Enemies/Boss/boss.cs
+++ Woofer/Systems/Enemies/Boss/boss.cs
@@ -7,12 +7,28 @@ using EntityComponentSystem.Components;
 using EntityComponentSystem.ComponentSystems;
 using EntityComponentSystem.Entities;
 using EntityComponentSystem.Events;
+using EntityComponentSystem.Util;
 
 namespace WooferGame.Systems.Enemies.Boss
 {
     [Component("boss")]
     class Boss : Component
     {
-        public double HoverTimer { get; internal set; }
+        public const int Circling = 1;
+        public const int Laser = 2;
+
+
+        public double HoverTimer { get; set; }
+        public int State { get; set; } = Circling;
+        public bool Transitioning { get; set; } = true;
+
+        public double StateData { get; set; }
+
+        public void ChangeState(int state)
+        {
+            State = state;
+            Transitioning = true;
+            StateData = 0;
+        }
     }
 }
